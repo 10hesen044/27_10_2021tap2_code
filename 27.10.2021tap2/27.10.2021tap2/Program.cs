dd46a0e03e1c7f1ee6dc36192e3386859347a0f6
10hesen044 <tu201906250@code.edu.az>
2021-10-27 12:05:55 +0400

    project added

---
+++ 27.10.2021tap2/27.10.2021tap2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _27._10._2021tap2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 48;
+
+            int i = 1;
+            while (i * i <= num) 
+            {
+                i++;
+            }
+            Console.WriteLine("result: " + (i-1));
+        }
+    }
+}
